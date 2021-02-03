8811505887047a259483b11428fef7f931d5ee88
john roque jorillo <jorillojohnroque@gmail.com>
2021-02-03 20:26:07 +0800

    Create feed item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by John Roque Jorillo on 2/3/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
