9897d9635525ca0223d15256afefd4d93c449743
john roque jorillo <jorillojohnroque@gmail.com>
2021-02-08 23:59:28 +0800

    Remove duplicated tests

--- EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
@@ -25,15 +25,15 @@ class URLSessionHTTPClient {
 class URLSessionHTTPClientTests: XCTestCase {
    
     // MARK: - Subclass mocking
-    func test_getFromURL_createsDataTaskWithURL() {
-        let url = URL(string: "http://any-url.com")!
-        let session = URLSessionSpy()
-        let sut = URLSessionHTTPClient(session: session)
-        
-        sut.get(from: url)
-        
-        XCTAssertEqual(session.receivedURLs, [url])
-    }
+//    func test_getFromURL_createsDataTaskWithURL() {
+//        let url = URL(string: "http://any-url.com")!
+//        let session = URLSessionSpy()
+//        let sut = URLSessionHTTPClient(session: session)
+//
+//        sut.get(from: url)
+//
+//        XCTAssertEqual(session.receivedURLs, [url])
+//    }
     
     func test_getFromURL_resumeDataTaskWithURL() {
         let url = URL(string: "http://any-url.com")!
