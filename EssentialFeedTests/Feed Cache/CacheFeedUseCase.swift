7d58f723a2baa3a59692a7dc3911198a729790fb
john roque jorillo <jorillojohnroque@gmail.com>
2021-02-14 06:38:35 +0800

    Invert if logic to make code paths easier to follow

--- EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
@@ -21,12 +21,14 @@ class LocalFeedLoader {
         store.deleteCachedFeed { [weak self] error in
             guard let self = self else { return }
             
-            if error == nil {
-                self.store.insert(items, timestamp: self.currentDate()) { error in
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            } else {
+                self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
+                    guard self != nil else { return }
+                    
                     completion(error)
                 }
-            } else {
-                completion(error)
             }
         }
     }
@@ -116,6 +118,20 @@ class CacheFeedUseCase: XCTestCase {
     
         sut = nil
         store.completeDeletion(with: anyNSError())
+        
+        XCTAssertTrue(receivedResults.isEmpty)
+    }
+    
+    func test_save_doesNotDeliverInsertionErrorAfterSUTInstanceHasBeenDeallocated() {
+        let store = FeedStoreSpy()
+        var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
+        
+        var receivedResults = [Error?]()
+        sut?.save([uniqueItem()]) { receivedResults.append($0) }
+        
+        store.completeDeletionSuccessFully()
+        sut = nil
+        store.completeInsertion(with: anyNSError())
         
         XCTAssertTrue(receivedResults.isEmpty)
     }
