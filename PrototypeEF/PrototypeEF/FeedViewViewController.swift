81e78f3b37a801df3736c772a437b75ba4921ddf
john roque jorillo <jorillojohnroque@gmail.com>
2021-03-07 10:32:54 +0800

    Add prototype storyboard with table view and feed cell layout

---
+++ PrototypeEF/PrototypeEF/FeedViewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FeedViewViewController.swift
+//  PrototypeEF
+//
+//  Created by John Roque Jorillo on 3/7/21.
+//
+
+import UIKit
+
+class FeedViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
