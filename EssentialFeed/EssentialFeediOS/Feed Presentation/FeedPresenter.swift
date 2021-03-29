c0eb01986543372d57a5e4fa47ce1cd3c07fb369
john roque jorillo <jorillojohnroque@gmail.com>
2021-03-30 00:12:36 +0800

    Render error message on load feed error

--- EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -15,13 +15,23 @@ protocol FeedView {
     func display(_ viewModel: FeedViewModel)
 }
 
+struct FeedErrorViewModel {
+    let message: String
+}
+
+protocol FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel)
+}
+
 final class FeedPresenter {
     private let feedView: FeedView
     private let loadingView: FeedLoadingView
+    private let errorView: FeedErrorView
     
-    init(feedView: FeedView, loadingView: FeedLoadingView) {
+    init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
         self.feedView = feedView
         self.loadingView = loadingView
+        self.errorView = errorView
     }
     
     static var title: String {
@@ -30,6 +40,13 @@ final class FeedPresenter {
                                  bundle: Bundle(for: FeedPresenter.self),
                                  comment: "Title for the feed view")
     }
+    
+    private var feedLoadError: String {
+            return NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
+                 tableName: "Feed",
+                 bundle: Bundle(for: FeedPresenter.self),
+                 comment: "Error message displayed when we can't load the image feed from the server")
+        }
     
     func didStartLoadingFeed() {
         guard Thread.isMainThread else {
@@ -59,6 +76,7 @@ final class FeedPresenter {
             }
         }
         
+        errorView.display(FeedErrorViewModel(message: feedLoadError))
         loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
 }
