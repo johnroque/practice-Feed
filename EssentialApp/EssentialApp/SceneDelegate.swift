8b2d727ac98565a3ccd9cfff7db3bc3a4750698e
john roque jorillo <jorillojohnroque@gmail.com>
2021-04-12 03:58:08 +0800

    Configure CI to run tests for the EssentialApp iOS application

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 import EssentialFeed
 import EssentialFeediOS
 
@@ -27,6 +28,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //        let feedViewController = FeedUIComposer.feedComposeWith(feedLoader: feedLoader, imageLoader: imageLoader)
 //        
 //        window?.rootViewController = feedViewController
+        
+//        let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5d1c78f21e661a0001ce7cfd/1562147059075/feed-case-study-v1-api-feed.json")!
+//
+//        let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+//        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
+//        let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
+//
+//        let localStoreURL = NSPersistentContainer
+//            .defaultDirectoryURL()
+//            .appendingPathComponent("feed-store.sqlite")
+//
+//        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
+//        let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
+//        let localImageLoader = LocalFeedImageDataLoader(store: localStore)
+//
+//        window?.rootViewController = FeedUIComposer.feedComposeWith(feedLoader: FeedLoaderWithFallbackComposite(primary: remoteFeedLoader, fallback: localFeedLoader), imageLoader: FeedImageDataLoaderWithFallbackComposite(primary: localImageLoader, fallback: remoteImageLoader))
     }
 
 }
