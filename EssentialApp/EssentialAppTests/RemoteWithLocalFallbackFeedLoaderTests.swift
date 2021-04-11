eed60858bd71744cb5e4dc82104948c94e7462a3
john roque jorillo <jorillojohnroque@gmail.com>
2021-04-12 01:20:21 +0800

    Configure EssentialApp scheme to execute tests in random order and gather coverage for the EssentialApp target

---
+++ EssentialApp/EssentialAppTests/RemoteWithLocalFallbackFeedLoaderTests.swift
@@ -0,0 +1,16 @@
+//
+//  RemoteWithLocalFallbackFeedLoaderTests.swift
+//  EssentialAppTests
+//
+//  Created by John Roque Jorillo on 4/11/21.
+//
+
+import XCTest
+
+class RemoteWithLocalFallbackFeedLoaderTests: XCTestCase {
+    
+    func test_load_deliversRemoteFeedOnremoteSuccess() {
+        
+    }
+    
+}
